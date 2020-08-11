34331f15d37676fb0d500a72ff7ceb6d02d2bb12
George Cheng <Gerhut@GMail.com>
2020-08-12 01:28:59 +0800

    Hello world

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# reactions.vercel.app
-Reactions badge on Vercel.
+# plus1.vercel.app
+
+[![Build Status](https://travis-ci.com/Gerhut/visitors.vercel.app.svg)](https://travis-ci.com/Gerhut/visitors.vercel.app)
+[![dependencies Status](https://david-dm.org/Gerhut/visitors.vercel.app/status.svg)](https://david-dm.org/Gerhut/visitors.vercel.app)
+[![devDependencies Status](https://david-dm.org/Gerhut/visitors.vercel.app/dev-status.svg)](https://david-dm.org/Gerhut/visitors.vercel.app?type=dev)
+[![JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+Reactions badge on [Vercel](https://vercel.com/).
+
+## License
+
+MIT
---
+++ api/index.js
@@ -0,0 +1,76 @@
+import { request } from '@octokit/request'
+import { RequestError } from '@octokit/request-error'
+import { makeBadge } from 'badge-maker'
+
+const { GITHUB_TOKEN } = process.env
+
+const REACTION_EMOJI = {
+  '+1': '👍',
+  '-1': '👎',
+  laugh: '😄',
+  confused: '😕',
+  heart: '❤️',
+  hooray: '🎉',
+  rocket: '🚀',
+  eyes: '👀'
+}
+
+/**
+ * @param {string} owner
+ * @param {string} repo
+ * @param {number} issueNumber
+ * @param {'+1'|'-1'|'laugh'|'confused'|'heart'|'hooray'|'rocket'|'eyes'} reactionType
+ * @returns {Promise<number>}
+ */
+async function getReactionCount (owner, repo, issueNumber, reactionType) {
+  const { data: issue } = await request('GET /repos/{owner}/{repo}/issues/{issue_number}', {
+    owner,
+    repo,
+    issue_number: issueNumber,
+    headers: {
+      authorization: GITHUB_TOKEN != null ? `token ${GITHUB_TOKEN}` : undefined
+    },
+    mediaType: {
+      previews: [
+        'squirrel-girl'
+      ]
+    }
+  })
+
+  console.log(issue)
+
+  return issue.reactions[reactionType]
+}
+
+/**
+ * @param {import('http').IncomingMessage} request
+ * @param {import('http').ServerResponse} response
+ */
+export default async function api (request, response) {
+  const { url } = request
+  const { pathname, searchParams } = new URL(url, 'http://localhost/')
+  const match = pathname.match(/^\/([A-Za-z0-9-]+)\/([A-Za-z0-9-_.]+)\/issues\/([1-9]\d*)(?:\/(\+1|-1|laugh|confused|heart|hooray|rocket|eyes))?$/)
+  if (match == null) {
+    return response.writeHead(400)
+      .end('Invalid URL')
+  }
+  const [, owner, repo, issueNumber, reactionType='+1'] = match
+  try {
+    const reactionCount = await getReactionCount(owner, repo, Number(issueNumber), reactionType)
+
+    const body = makeBadge({
+      label: searchParams.has('label') ? searchParams.get('label') : REACTION_EMOJI[reactionType],
+      message: String(reactionCount)
+    })
+    response.writeHead(200, {
+      'Content-Type': 'image/svg+xml'
+    }).end(body)
+  } catch (error) {
+    console.error(error)
+    if (error instanceof RequestError) {
+      response.writeHead(error.status).end(error.message)
+    } else {
+      response.writeHead(500).end(error.message)
+    }
+  }
+}
